e6401a82e6119e1820d50b18313f5ad5c6c72b9e
Griffin White <GManyo123@gmail.com>
2020-11-05 18:18:43 -0500

    Added labels, texfields, buttons, and togglebuttons to display!

--- GitHubAutomaticPush/src/LogInScreen.java
+++ GitHubAutomaticPush/src/LogInScreen.java
@@ -1,13 +1,28 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
 //import 
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JToggleButton;
 
 public class LogInScreen extends JPanel{
 	
 	String filePath;
 	//GitHub github
+	
+	private JLabel infoLabel, usernameLabel, passwordLabel, feedbackLabel;
+	
+	private JTextField usernameTextField, passwordTextField;
+	
+	private JButton submitButton;
+	
+	private JToggleButton darkModeToggleButton; //used to toggle between dark mode and light mode
+	
+	private Font bigWords; //used to resize the text
 
 	public LogInScreen(int height, int width, Color bgColor) {
 		setPreferredSize(new Dimension(width, height));
@@ -15,5 +30,29 @@ public class LogInScreen extends JPanel{
 
 		this.setFocusable(true);
 		this.setFocusTraversalKeysEnabled(false);
+		
+		infoLabel = new JLabel("Please input your github credentials below.");
+		usernameLabel = new JLabel("Username: ");
+		passwordLabel = new JLabel("Password: ");
+		feedbackLabel = new JLabel("");
+		
+		usernameTextField = new JTextField("Username...");
+		passwordTextField = new JTextField("Password...");
+		
+		submitButton = new JButton("submit");
+		
+		darkModeToggleButton = new JToggleButton("Toggle Dark Mode Theme");
+		
+		bigWords = new Font("Serif", Font.PLAIN, width/30); //Makes it so the text is easily seeable, using one 30th of the window's width for the font size
+		
+		infoLabel.setFont(bigWords);
+		usernameLabel.setFont(bigWords);
+		passwordLabel.setFont(bigWords);
+		feedbackLabel.setFont(bigWords);
+		usernameTextField.setFont(bigWords);
+		passwordTextField.setFont(bigWords);
+		submitButton.setFont(bigWords);
+		darkModeToggleButton.setFont(bigWords);
+		
 	}
 }
