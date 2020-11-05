4c696a5cd213be0f33ce7c265a5c8fd86b04faef
Breanna <bgaran21@jcu.edu>
2020-11-05 08:24:40 -0500

    add jpanel and helper classes

---
+++ GitHubAutomaticPush/src/GitHubHelper.java
@@ -0,0 +1,22 @@
+
+public class GitHubHelper {
+
+	/**
+	 * This method connects to GitHub with the given user name and password
+	 * @param username - GitHub User Name (String)
+	 * @param password - GitHub Password (String)
+	 */
+	public void ConnectToGitHub(String username, String password)
+	{
+		//TODO
+	}
+	
+	/**
+	 * This method saves the currenct progress and
+	 * pushes the current project to the connected GitHub repository
+	 */
+	public void githubPush()
+	{
+	//TODO	
+	}
+}
---
+++ GitHubAutomaticPush/src/LogInScreen.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+//import 
+import javax.swing.JPanel;
+
+public class LogInScreen extends JPanel{
+	
+	String filePath;
+	//GitHub github
+
+	public LogInScreen(int height, int width, Color bgColor) {
+		
+	}
+}
