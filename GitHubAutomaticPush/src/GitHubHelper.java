25fc2368b162a74769352884889d05c6b2013496
Courtney <ccrawford21@jcu.edu>
2020-12-01 18:14:48 -0500

    git diff

--- GitHubAutomaticPush/src/GitHubHelper.java
+++ GitHubAutomaticPush/src/GitHubHelper.java
@@ -10,6 +10,10 @@ public class GitHubHelper {
 	String projectName = "SeniorDesignTemplateProject";	//will need to be user input
 	String repoPath = "https://github.com/bgaran/SeniorDesignTemplateProject.git";	//if using existing repo, will need to be user input
 	
+	//just use this to call the method, not needed in final
+	//GitHubHelper() {
+		//githubDiff();
+	//}
 		
 	/**
 	 * This method connects to GitHub with the given user name and password
@@ -35,6 +39,27 @@ public class GitHubHelper {
 						//+ " && git config --global user.email \"ccrawford21@jcu.edu\"" 
 						//+ " && git config --global user.name \"Courtney\""
 						+ "\" && git add . && git commit -m \"Autocommit\" && git push\"");
+	            
+	        } catch (IOException e) {
+	            System.out.println(e.toString());
+	        }
+		}
+	}
+	
+	/***
+	 * This method will compare the most recent version of code in the repo with what the user has
+	 * in their local and show it in the command prompt/terminal window. From there, the user can identify
+	 * and differences and manually change them in their local
+	 * 
+	 * Red is what is currently in the repository
+	 * Green is any differences the user has compared to the repository
+	 */
+	public void githubDiff() {
+		if (os.contains("Win")) {
+			//TODO: not focus on the terminal
+			try {
+				Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"cd ../"+projectName+"\""
+						+ "\" && git diff\"");
 	            
 	        } catch (IOException e) {
 	            System.out.println(e.toString());
