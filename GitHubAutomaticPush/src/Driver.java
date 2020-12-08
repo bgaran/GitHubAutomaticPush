e23db44dbe4013787a5245cf20548b7915fe279c
Breanna <bgaran21@jcu.edu>
2020-12-08 18:49:11 -0500

    remove testing code from driver

--- GitHubAutomaticPush/src/Driver.java
+++ GitHubAutomaticPush/src/Driver.java
@@ -9,8 +9,6 @@ public class Driver {
 	public static void main(String[] args) {
 		//manages frames in another class made to do so, this is so staticitis doesnt occur
 		FrameManager frameManager = new FrameManager();
-		GitHubHelper helper=new GitHubHelper();
-		helper.gitClone("/Users/breannagaran/Desktop/CS435/", "https://github.com/bgaran/TestClone.git");
 	}
 
 }
