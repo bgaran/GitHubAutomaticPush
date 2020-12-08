ce08a02fc6fe2e8ac0e6415fe224c1edc249dbe1
Breanna <bgaran21@jcu.edu>
2020-12-08 18:48:26 -0500

    Add gitClone method (clones the given repository to a given file path- must then manually navigate to File> Open project from file system> and then locate the repository you just created locally.  This will then add it to package explorer and git repositories and will be ready for push and pull with application).
    also added author names to github helper

--- GitHubAutomaticPush/src/Driver.java
+++ GitHubAutomaticPush/src/Driver.java
@@ -9,6 +9,8 @@ public class Driver {
 	public static void main(String[] args) {
 		//manages frames in another class made to do so, this is so staticitis doesnt occur
 		FrameManager frameManager = new FrameManager();
+		GitHubHelper helper=new GitHubHelper();
+		helper.gitClone("/Users/breannagaran/Desktop/CS435/", "https://github.com/bgaran/TestClone.git");
 	}
 
 }
