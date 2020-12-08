948a90851594a18ac0a4a22bd14e1002392e4d7b
Griffin White <GManyo123@gmail.com>
2020-12-08 17:14:47 -0500

    setback to driver, nothing much to say

--- GitHubAutomaticPush/src/Driver.java
+++ GitHubAutomaticPush/src/Driver.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
@@ -7,6 +8,8 @@ import javax.swing.JFrame;
 public class Driver {
 
 	public static void main(String[] args) {
+		
+		
 		//manages frames in another class made to do so, this is so staticitis doesnt occur
 		FrameManager frameManager = new FrameManager();
 	}
